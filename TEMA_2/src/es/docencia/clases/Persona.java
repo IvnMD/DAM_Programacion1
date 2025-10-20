d5c5c9e8aebece5c88478c6bf664177d0e5c64b3
Ivan Mesa <ivan.mesa.dominguez@gmail.com>
2025-10-20 15:06:44 +0100

    Subida comienzo clase 20/10/25

---
+++ TEMA_2/src/es/docencia/clases/Persona.java
@@ -0,0 +1,27 @@
+/**
+ * Clase persona que almacena la informacion de esta
+ * @author Iván Mesa
+ * @version 1.0.0
+ * @date 20/10/25
+ * @bugs No bugs known.
+ */
+public class Persona {
+
+    private String fechaNacimiento;
+    private String nombre;
+    private String apellido1;
+    private Strgins apellido2;
+
+    /**
+     * Contructor por defecto
+     */
+    public Persona() {};
+    /**
+     * Constructor de la clase persona
+     * @param nombre Parametro para el nombre de la clase
+     */
+    public Persona(String nombre){
+        this.nombre; 
+    }
+
+}

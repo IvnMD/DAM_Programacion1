3729d18bc03775e6e42cdc0b287bcd8709165ce6
Ivan Mesa <ivan.mesa.dominguez@gmail.com>
2025-10-24 16:24:09 +0100

    Repaso previo a examen 24/1025

---
+++ TEMA_2/src/es/docencia/clases/ejerciciosClases/Programa8/Direccion.java
@@ -0,0 +1,76 @@
+package es.docencia.clases.ejerciciosClases.Programa8;
+import java.util.Objects;
+
+/**
+ * @author IvnMD
+ * @since 24/10/25
+ * @version 1.0
+ * @brief Clase direccion ejercicio 8
+ * @bugs sin bugs conocidos
+ */
+
+public class Direccion {
+
+    private String calle;
+    private int numero;
+    private String ciudad;
+    /**
+     * Constructor vacio 
+     */
+    Direccion (){};
+    /**
+     * Constructor por parametros de la clase Direccion
+     * @param calle 
+     * @param numero
+     * @param ciudad
+     */
+    Direccion(String calle, int numero, String ciudad){
+        this.calle = calle;
+        this.numero = numero;
+        this.ciudad = ciudad;
+    }
+
+
+    // Getters y setters
+    public String getCalle() {
+        return this.calle;
+    }
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+    public int getNumero() {
+        return this.numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public String getCiudad() {
+        return this.ciudad;
+    }
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+    
+    // toString legible
+    @Override
+    public String toString() {
+        return "{" +
+            " calle='" + getCalle() + "'" +
+            ", numero='" + getNumero() + "'" +
+            ", ciudad='" + getCiudad() + "'" +
+            "}";
+    }
+        // equals: ajusta los campos usados en la comparación
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Direccion)) {
+            return false;
+        }
+        Direccion direccion = (Direccion) o;
+        return Objects.equals(calle, direccion.calle) && numero == direccion.numero && Objects.equals(ciudad, direccion.ciudad);
+    }
+
+
+}
---
+++ TEMA_2/src/es/docencia/clases/ejerciciosClases/Programa8/Programa8.java
@@ -0,0 +1,14 @@
+package es.docencia.clases.ejerciciosClases.Programa8;
+
+public class Programa8 {
+    public static void main(String[] args) {
+        Direccion direccion1 = new Direccion("Gran via", 10, "Madrid");
+        Direccion direccion2 = new Direccion("Gran via", 10, "Madrid");
+
+        System.out.println("Vivo en " + direccion1.getCalle() + " " + direccion1.getNumero() + ", " + direccion1.getCiudad());
+        System.out.println(direccion1.equals(direccion2));
+        direccion2.setCiudad("Barcelona");
+        System.out.println(direccion2);
+        System.out.println(direccion1.equals(direccion2));
+    }
+}

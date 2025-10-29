4839e1f3529e90e500ad60fb2cbd6ef098cddb51
Ivan Mesa <ivan.mesa.dominguez@gmail.com>
2025-10-29 18:59:00 +0000

    Subida de principio de clase 29/10/25

---
+++ TEMA_2/docencia-clases/src/main/java/com/docencia/clases/IntegerUtils.java
@@ -0,0 +1,23 @@
+package com.docencia.clases;
+/**
+ * @author IvnMD
+ * @version 1.0.0
+ * 
+ */
+public final class IntegerUtils {
+    private IntegerUtils() {}
+
+    public static String bin(int x) {
+        return null;
+    }
+
+
+    public static int popcount(int x) {
+         return 0;
+        }
+
+
+    public static int comparaSinSigno(int a, int b) { 
+        return 0;
+    }
+}
---
+++ TEMA_2/docencia-clases/src/main/java/com/docencia/clases/StringUtils.java
@@ -0,0 +1,32 @@
+package com.docencia.clases;
+
+/**
+ * @author IvnMD
+ * @version 1.0.0
+ * 
+ */
+
+public class StringUtils {
+    /**
+     * Funcion que normaliza una cadena de texto
+     * @param cadena String que pretende normalizar
+     * @return String de texto normalizado
+     */
+    public static String normalizaNombre(String cadena) {
+
+        return null;
+    }
+
+    public static int[] cuentaLetras(String cadena) {
+
+        return null;
+    }
+
+    public static boolean esPalindromo(String cadena) {
+        return false;
+    }
+
+    public static String acronimo(String frase) {
+        return null;
+    }
+}

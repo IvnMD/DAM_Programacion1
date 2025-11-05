fdd74515f9f849b4a603fdba27e9d296107de474
Ivan Mesa <ivan.mesa.dominguez@gmail.com>
2025-11-05 20:26:22 +0000

    Ejercicios de repaso trabajados en clase 05/11/25

--- TEMA_2/docencia-programacion-repaso-I/src/main/java/com/docencia/programacion/Ejercicio9.java
+++ TEMA_2/docencia-programacion-repaso-I/src/main/java/com/docencia/programacion/Ejercicio9.java
@@ -1,18 +1,38 @@
 package com.docencia.programacion;
 
 public class Ejercicio9 {
+    /**
+     * Funcion que calcula el valor absoluto
+     * @param x Valor de entrada (tipo int)
+     * @return Valor absoluto
+     */
     public static int absValue(int x) {
-        // TODO implementar
-        return 0;
+        return Math.abs(x);
     }
-
+    /**
+     * Funcion que calculo el maximo de dos numero
+     * @param a Primer valor de entrada
+     * @param b Segundo valor de entrada
+     * @return Devuelve el valor mas alto de los dos valores introducidos
+     */
     public static int maxOfTwo(int a, int b) {
-        // TODO implementar
-        return 0;
-    }
+        // if (a<b){
+        //     return a;  //! Metodo de picar codigo
+        // }
+        //     return b;
 
+        return Math.max(a,b);
+    }
+    /**
+     * Metodo que calcula la distancia entre dos puntos
+     * @param x1 Posicion en el eje X del punto 1
+     * @param y1 Posicion en el eje y del punto 1
+     * @param x2 Posicion en el eje X del punto 2
+     * @param y2 Posicion en el eje y del punto 2
+     * @return Distancia entre los dos puntos en un plano 2D
+     */
     public static double distance2D(double x1, double y1, double x2, double y2) {
-        // TODO implementar
-        return 0.0;
+        double Distancia = Math.sqrt(Math.pow(x1-x2,2) + Math.pow(y1-y2,2));
+        return Distancia;
     }
 }

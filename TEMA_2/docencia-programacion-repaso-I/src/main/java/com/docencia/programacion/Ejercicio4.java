465848e7be77b1dfe5b07945a055dca24f95c668
IvnMD <ivan.mesa.dominguez@gmail.com>
2025-11-07 12:07:16 +0000

    Expo

--- TEMA_2/docencia-programacion-repaso-I/src/main/java/com/docencia/programacion/Ejercicio4.java
+++ TEMA_2/docencia-programacion-repaso-I/src/main/java/com/docencia/programacion/Ejercicio4.java
@@ -8,6 +8,12 @@ package com.docencia.programacion;
  */
 
 public class Ejercicio4 {
+    /**
+     * Calcula el area de un rectangulo.
+     * @param base Base del rectangulo.
+     * @param height Altura del rectangulo.
+     * @return Area del rectangulo. Si la base o la altura son menores o iguales a 0, devuelve 0.
+     */
     public static int area(int base, int height) {
         if (base <= 0 || height <=0){
             return 0;
@@ -15,9 +21,16 @@ public class Ejercicio4 {
         int resultado = base * height;
         return resultado;
     }
-
+    /**
+     * Calcula el perimetro de un rectangulo.
+     * @param base Base
+     * @param height Altura
+     * @return Perimetro del rectangulo. Si la base o la altura son menores a 0, devuelve 0.
+     */
     public static int perimeter(int base, int height) {
-        // TODO implementar
-        return -1;
+            if (base < 0 || height < 0){
+            return 0;
+        }
+        return (base*2+height*2);
     }
 }
--- TEMA_2/docencia-programacion-repaso-I/src/main/java/com/docencia/programacion/Ejercicio6.java
+++ TEMA_2/docencia-programacion-repaso-I/src/main/java/com/docencia/programacion/Ejercicio6.java
@@ -2,7 +2,7 @@ package com.docencia.programacion;
 
 public class Ejercicio6 {
     public static double calculateFinalPrice(double basePrice, double taxRate) {
-        // TODO implementar
+        
         return 0.0;
     }
 }

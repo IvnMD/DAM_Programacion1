b773f7cbdca42d21e531e29869dd194f0a2ae793
Iván Mesa Dominguez <ivan.mesa.dominguez@gmail.com>
2025-11-12 19:51:36 +0100

    Espero que esto no se suba

--- TEMA_2/docencia-programacion-repaso-I/src/main/java/com/docencia/programacion/Ejercicio4.java
+++ TEMA_2/docencia-programacion-repaso-I/src/main/java/com/docencia/programacion/Ejercicio4.java
@@ -28,9 +28,17 @@ public class Ejercicio4 {
      * @return Perimetro del rectangulo. Si la base o la altura son menores a 0, devuelve 0.
      */
     public static int perimeter(int base, int height) {
+<<<<<<< HEAD
             if (base < 0 || height < 0){
             return 0;
         }
         return (base*2+height*2);
+=======
+        if (base <= 0 || height <=0){
+            return 0;
+        }
+        // int resultado = base * height;
+        return (base * height) * 2;
+>>>>>>> 3071fa5 (Espero que esto no se suba)
     }
 }

6545fe77eec1fa6784439009862760f068ef6456
Ivan Mesa <ivan.mesa.dominguez@gmail.com>
2025-12-12 18:46:59 +0000

    clase 12/12/25

---
+++ TEMA_3/ejercicios-1-eval-repaso-I/ejercicios-1-eval-repaso-I/src/main/java/com/docencia/listas/ejercicio3/Ejercicio3.java
@@ -0,0 +1,32 @@
+package com.docencia.listas.ejercicio3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * EJERCICIO 3 (ESQUELETO) - Intersección ordenada.
+ */
+public class Ejercicio3 {
+
+    private Ejercicio3() {
+    }
+
+    /**
+     * TODO: Implementar completamente según enunciado y tests.
+     */
+    public static List<Integer> interseccionOrdenada(List<Integer> a, List<Integer> b) {
+
+        if(a ==null || a.isEmpty() || b==null || b.isEmpty()){
+            return new ArrayList<>();
+        }
+
+        List<Integer> resultado = new ArrayList<>();
+        for (Integer numero : a) {
+            if(b.contains(numero) && !resultado.contains(numero)){
+                resultado.add(numero);
+            }
+        }
+        
+        return resultado;
+    }
+}

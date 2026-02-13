3ca1f615cd295d0b536a8535d882701e726ee98b
Iván Mesa <ivan.mesa.dominguez@gmail.com>
2026-02-13 12:25:30 +0000

    13/02/26

--- TEMA_5/Tareas Evaluables/TE3.8/tarea-calificable-3.8/src/main/java/com/docencia/expresiones/ejercicio6/Ejercicio6.java
+++ TEMA_5/Tareas Evaluables/TE3.8/tarea-calificable-3.8/src/main/java/com/docencia/expresiones/ejercicio6/Ejercicio6.java
@@ -8,9 +8,57 @@ import java.util.regex.Pattern;
 
 public final class Ejercicio6 {
 
-    private Ejercicio6() {}
+    private Ejercicio6() {
+    }
 
     public static LocalDateTime applyDeltaCommand(LocalDateTime base, String command) {
-        return null;
+        if (base == null || command == null || command.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+        String patron = "^([\\+\\-])([0-9]+)([hdmyHDMY])$";
+        Pattern pattern = Pattern.compile(patron);
+        Matcher matcher = pattern.matcher(command);
+
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException();
+        }
+
+        String signo = matcher.group(1);
+        int cantidad = Integer.parseInt(matcher.group(2));
+        char unidad = matcher.group(3).charAt(0);
+        LocalDateTime resultado;
+        switch (unidad) {
+            case 'Y':
+            case 'y':
+                if (signo.equals("+")) {
+                    return base.plus(Period.ofYears(cantidad));
+                } else {
+                    return base.minus(Period.ofYears(cantidad));
+                }
+            case 'd':
+            case 'D':
+                if (signo.equals("+")) {
+                    return base.plus(Period.ofDays(cantidad));
+                } else {
+                    return base.minus(Period.ofDays(cantidad));
+                }
+            case 'h':
+            case 'H':
+                if (signo.equals("+")) {
+                    return base.plus(Duration.ofHours(cantidad));
+                } else {
+                    return base.minus(Duration.ofHours(cantidad));
+                }
+            case 'm':
+            case 'M':
+                if (signo.equals("+")) {
+                    return base.plus(Duration.ofMinutes(cantidad));
+                } else {
+                    return base.minus(Duration.ofMinutes(cantidad));
+                }
+            default:
+                throw new IllegalArgumentException();
+
+        }
     }
 }

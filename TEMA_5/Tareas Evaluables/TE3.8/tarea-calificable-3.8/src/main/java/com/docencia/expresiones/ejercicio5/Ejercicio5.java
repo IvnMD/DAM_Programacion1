db5df8ab9b642fbd37eb8befabc8229f2360f1ae
IvnMD <ivan.mesa.dominguez@gmail.com>
2026-02-13 06:19:51 +0000

    TE 3.8

--- TEMA_5/Tareas Evaluables/TE3.8/tarea-calificable-3.8/src/main/java/com/docencia/expresiones/ejercicio5/Ejercicio5.java
+++ TEMA_5/Tareas Evaluables/TE3.8/tarea-calificable-3.8/src/main/java/com/docencia/expresiones/ejercicio5/Ejercicio5.java
@@ -10,6 +10,14 @@ public final class Ejercicio5 {
     private Ejercicio5() {}
 
     public static Instant parseLogInstant(String logLine) {
-         return null;
+        if (logLine == null || logLine.isBlank()){
+            throw new IllegalArgumentException();
+        } 
+        String anyo = "[0-9]{4}";
+        String mes = "(0[1-9]|1[0-2])";
+        String dia = "(0[1-9]|[12][0-9]|3[01])";
+        String hora = "(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-9]{2}+[0-9]{2}:[0-9]{2}";
+        
+        return null;
     }
 }

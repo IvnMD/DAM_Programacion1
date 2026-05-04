5b5a22ccd5ae5a065228a7e89a8b3da0b73a482d
Iván Mesa <ivan.mesa.dominguez@gmail.com>
2026-05-04 14:50:28 +0100

    04/05/26

--- repaso-por-bloques 2/repaso-por-bloques/src/main/java/com/docencia/algoritmos/OrdenacionServiceImpl.java
+++ repaso-por-bloques 2/repaso-por-bloques/src/main/java/com/docencia/algoritmos/OrdenacionServiceImpl.java
@@ -2,6 +2,7 @@ package com.docencia.algoritmos;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -14,6 +15,7 @@ public class OrdenacionServiceImpl implements OrdenacionService {
             throw new IllegalArgumentException();
         }
         List<Integer> resultado = new ArrayList<>(numeros);
+        // resultado.sort(null); //! Ascendente: [1, 2, 5, 8]
         for (int i = 0; i < resultado.size() - 1; i++) {
             for (int j = 0; j < resultado.size() - i - 1; j++) {
                 if (resultado.get(j) > resultado.get(j + 1)) {
@@ -33,6 +35,7 @@ public class OrdenacionServiceImpl implements OrdenacionService {
             throw new IllegalArgumentException();
         }
         List<Integer> resultado = new ArrayList<>(numeros);
+        // resultado.sort(Comparator.reverseOrder()); //! Descendente: [8, 5, 2, 1]
         for (int i = 0; i < resultado.size() - 1; i++) {
             for (int j = 0; j < resultado.size() - i - 1; j++) {
                 if (resultado.get(j) < resultado.get(j + 1)) {
@@ -54,24 +57,41 @@ public class OrdenacionServiceImpl implements OrdenacionService {
         List<String> resultado = new ArrayList<>(palabras);
         resultado.sort(String.CASE_INSENSITIVE_ORDER);
         return resultado;
-        
-        //! return new ArrayList<>(new TreeSet<>(palabras)); 
-        //? Ventajas: Ordena automáticamente, elimina duplicados.
-        //? Desventaja: No permite elementos repetidos.
-        //! Set<String> sorted = new TreeSet<>((a, b) -> a.compareToIgnoreCase(b));
-        //? Para ignorar las mayusculas 
+
+        // ! return new ArrayList<>(new TreeSet<>(palabras));
+        // ? Ventajas: Ordena automáticamente, elimina duplicados.
+        // ? Desventaja: No permite elementos repetidos.
+        // ! Set<String> sorted = new TreeSet<>((a, b) -> a.compareToIgnoreCase(b));
+        // ? Para ignorar las mayusculas
 
     }
 
     @Override
     public List<Integer> invertirLista(List<Integer> numeros) {
-l,lklkkkkk,m   
+        if (numeros == null || numeros.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        List<Integer> resultado = new ArrayList<>(numeros);
+        Collections.reverse(resultado);
+
+        return resultado;
+
     }
 
     @Override
     public Boolean estaOrdenadaAscendente(List<Integer> numeros) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'estaOrdenadaAscendente'");
+        if (numeros == null || numeros.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        List<Integer> resultado = new ArrayList<>(numeros);
+        resultado.sort(null);
+        for (int i = 0; i < numeros.size() - 1; i++) {
+            if (numeros.get(i) > numeros.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+
     }
 
 }
--- repaso-por-bloques 2/repaso-por-bloques/src/main/java/com/docencia/cadenas/StringServiceImpl.java
+++ repaso-por-bloques 2/repaso-por-bloques/src/main/java/com/docencia/cadenas/StringServiceImpl.java
@@ -4,38 +4,94 @@ public class StringServiceImpl implements StringService {
 
     @Override
     public String normalizarTexto(String texto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'normalizarTexto'");
+        if (texto == null || texto.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+        texto = texto.replaceAll("\\s+", " ").trim().toLowerCase();
+
+        return texto;
+
     }
 
     @Override
     public Boolean esPalindromo(String texto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'esPalindromo'");
+        if (texto == null || texto.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+
+        String normalizado = texto.replaceAll("\\s+", "").toLowerCase();
+        String textoInvertido = new StringBuilder(normalizado).reverse().toString();
+
+        return normalizado.equals(textoInvertido);
+
+        // ! CON UN FOR:
+        // for (int i = 0; i < texto.length() / 2; i++) {
+        // int j = texto.length() - 1 - i;
+        // if (texto.charAt(i) != texto.charAt(j)) {
+        // return false;
+        // }
+        // }
+        // return true;
     }
 
     @Override
     public Integer contarVocales(String texto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'contarVocales'");
+        if (texto == null || texto.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+
+        Integer contador = 0;
+        String vocales = "aeiouAEIOU";
+
+        for (int i = 0; i < texto.length(); i++) {
+            if (vocales.indexOf(texto.charAt(i)) != -1) {
+                contador++;
+            }
+            // char c = Character.toLowerCase(texto.charAt(i)); // !ALTERNATIVA
+            // if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            // contador++;
+            // }
+        }
+        return contador;
+
     }
 
     @Override
     public String extraerIniciales(String nombreCompleto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'extraerIniciales'");
+        if (nombreCompleto == null || nombreCompleto.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+        String[] palabras = nombreCompleto.split(" ");
+        String resultado = "";
+        for (String palabra : palabras) {
+            resultado += palabra.charAt(0);
+        }
+        return resultado.toUpperCase();
     }
 
     @Override
     public String invertirTexto(String texto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'invertirTexto'");
+        if (texto == null || texto.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+        String textoInvertido = new StringBuilder(texto).reverse().toString();
+        return textoInvertido;
     }
 
     @Override
     public Boolean contieneSoloLetras(String texto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'contieneSoloLetras'");
+        if (texto == null || texto.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+
+        String numeros = "0123456789";
+
+        for (int i = 0; i < texto.length(); i++) {
+            if (numeros.indexOf(texto.charAt(i)) != -1) {
+                return false;
+            }
+        }
+        return true;
     }
-    
+
 }

abd9f1ebf1f7e4bbd58ea51e750a13fe80410e8f
IvnMD <ivan.mesa.dominguez@gmail.com>
2026-05-08 04:03:34 +0100

    .

--- repaso-por-bloques/src/main/java/com/docencia/ficheros/CsvServiceImpl.java
+++ repaso-por-bloques/src/main/java/com/docencia/ficheros/CsvServiceImpl.java
@@ -1,5 +1,9 @@
 package com.docencia.ficheros;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,38 +16,113 @@ public class CsvServiceImpl implements CsvService {
 
     @Override
     public List<String> leerLineasCsv(Path ruta) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'leerLineasCsv'");
+        if (ruta == null) {
+            throw new IllegalArgumentException();
+        }
+        if (!Files.exists(ruta)) {
+            throw new IllegalArgumentException();
+        }
+
+        List<String> lineas = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(ruta.toFile()))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                lineas.add(linea);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return lineas;
     }
 
     @Override
     public List<String[]> leerRegistrosCsv(Path ruta) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'leerRegistrosCsv'");
+        if (ruta == null) {
+            throw new IllegalArgumentException();
+        }
+        if (!Files.exists(ruta)) {
+            throw new IllegalArgumentException();
+        }
+
+        List<String[]> registros = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(ruta.toString()))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                registros.add(linea.split(","));
+            }
+            return registros;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void escribirLineasCsv(Path ruta, List<String> lineas) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'escribirLineasCsv'");
+        if (ruta == null || !Files.exists(ruta) || lineas == null || lineas.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        List<String> filas = new ArrayList<>();
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(ruta.toString()))) {
+            for (String fila : filas) {
+                bw.write(fila);
+                bw.newLine();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+
+        }
     }
 
     @Override
     public Integer contarRegistrosCsv(Path ruta) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'contarRegistrosCsv'");
+        Integer resultado = 0;
+        return leerRegistrosCsv(ruta).size();
+
     }
 
     @Override
     public List<String[]> filtrarRegistrosPorValor(Path ruta, Integer columna, String valor) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'filtrarRegistrosPorValor'");
+        if (ruta == null || columna == null || valor == null || columna < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (!Files.exists(ruta)) {
+            throw new IllegalArgumentException();
+        }
+
+        List<String[]> registrosFiltrados = new ArrayList<>();
+        List<String[]> todos = leerRegistrosCsv(ruta);
+
+        for (String[] registro : todos) {
+            if (registro.length > columna &&
+                    (registro[columna] != null && registro[columna].equals(valor))) {
+                registrosFiltrados.add(registro);
+            }
+        }
+        return registrosFiltrados;
     }
 
     @Override
     public Map<String, Integer> contarFrecuenciaColumna(Path ruta, Integer columna) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'contarFrecuenciaColumna'");
+        if (ruta == null || columna == null || columna < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (!Files.exists(ruta)) {
+            throw new IllegalArgumentException();
+        }
+
+        Map<String, Integer> frecuencia = new LinkedHashMap<>(); // Preserva orden
+        List<String[]> registros = leerRegistrosCsv(ruta);
+
+        for (String[] registro : registros) {
+            if (registro.length > columna) {
+                String valor = registro[columna];
+                frecuencia.put(valor, frecuencia.getOrDefault(valor, 0) + 1);
+            }
+        }
+
+        return frecuencia;
     }
-    
 }
